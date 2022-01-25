134e24ebf0697a38c587f6fd33eacb17e35c4950
Michael Marzec <michaelmarzec11@gmail.com>
2022-01-24 19:00:13 -0700

    table_formatting

--- flask/flask-main.py
+++ flask/flask-main.py
@@ -1,6 +1,8 @@
 # https://15xu0h4j6i.execute-api.us-east-2.amazonaws.com/dev
 
 from flask import Flask, render_template
+from flask_table import Table, Col, LinkCol
+from flask import Flask, request, url_for
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -17,14 +19,27 @@ import pandas as pd
 
 ## Functions ##
 
+def format_column_names(df):
+
+	return df
+
 def data_ingest(file_name='age_tracking.csv'):
 	df = pd.read_csv(file_name)
 	return df
 
-# def data_plot_export(df, age_var, export_file_name='static/avg_age_plot.png', date_var = 'date', team_var = 'Team_Name'):
-# 	# df[date_var] = pd.to_datetime(df[date_var])
-# 	df.pivot(index="date", columns="Team_Name", values="Average_Age").plot(figsize=(20,10)).legend(loc='center',bbox_to_anchor=(1.0, 0.5))
-# 	plt.savefig(export_file_name, format='png')
+def df_currentDate_operations(df):
+	# reduce to current date
+	df_current = (df['date'] == df['date'].max())
+	df_current = df[df_current]
+	del df_current['date']
+	df_current = df_current.reset_index(drop=True)
+	df_current.index += 1
+
+	df_current.insert(0, 'id', range(1, 1 + len(df_current)))
+
+	# column names
+	return df_current
+
 
 ## Hard Codes ##
 df_columns_ind0 = ['Player','Age','Pos','GP','MIN','MPG','Usage']
@@ -46,20 +61,57 @@ def main():
 
 	df_final = data_ingest()
 
-	# Local Exports (Data & Plots)
-	# data_plot_export(df_final, 'Average_Age')
-
-	# Print Current DF
-	print(df_final)
+	# Cleanse for current averages
+	df_current = df_currentDate_operations(df_final)
 	
-	# Flask return
-	return render_template('view.html',  tables=[df_final.to_html(classes='data', header='true')])
+	# return render_template('view.html',  tables=[df_current.to_html(classes='mystyle',header='true', justify='center')])
+	return df_current
+
+
+class SortableTable(Table): # https://github.com/plumdog/flask_table/blob/master/examples/sortable.py # https://stackoverflow.com/questions/43552740/best-way-to-sort-table-based-on-headers-using-flask
+    id = Col('id')
+    Team_Name = Col('Team_Name')
+    Average_Age = Col('Average_Age')
+    allow_sort = True
+
+    def sort_url(self, col_key, reverse=False):
+        if reverse:
+            direction = 'desc'
+        else:
+            direction = 'asc'
+        return url_for('index', sort=col_key, direction=direction)
 
 
 ## Main Execution ##
 @app.route('/', methods=['GET','POST'])
-def execute():
-	return main()
+def index():
+	df = main()
+	sort = request.args.get('sort', 'id')
+	reverse = (request.args.get('direction', 'asc') == 'desc')
+	df = df.sort_values(by=[sort], ascending=reverse) ## issue
+	output_dict = df.to_dict(orient='records')
+	table = SortableTable(output_dict,
+                          sort_by=sort,
+                          sort_reverse=reverse)
+	# return table.__html__()
+	return render_template('view.html',  table=table.__html__())
+	# return render_template('view.html',  tables=table.__html__())#(classes='mystyle',header='true', justify='center'))#])
+	# sort = request.args.get('sort', 'id')
+	# reverse = (request.args.get('direction', 'asc') == 'desc')
+
+	# print(df)
+
+	# df = df.sort_values(by=[sort], ascending=reverse)
+	# output_dict = df.to_dict(orient='records')
+
+	# table = SortableTable(output_dict, sort_by=sort, sort_reverse=reverse)
+	# return table.__html__()
+	# df = main()
+	# sort = request.args.get('sort', 'id')
+	# reverse = (request.args.get('direction', 'asc') == 'desc')
+	# table = SortableTable(df.get_sorted_by(sort, reverse),sort_by=sort,sort_reverse=reverse)
+	# return table.__html__()
+
 
 # @app.after_request
 # def add_header(response):
